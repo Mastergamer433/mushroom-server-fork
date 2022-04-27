bfe58a576998fdce7b6f59efbae7a2001bfcb5f4
Elis Odenhage <teknikgillaren@gmail.com>
2022-04-27 14:35:29 +0200

    app.js

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+require("dotenv").config();
+
+app.use(cors());
+
+app.use("/api/v1", require("../routes/v1"));
+
+app.listen(8080, () => {
+  console.log("Example app listening on port 3000!");
+});
